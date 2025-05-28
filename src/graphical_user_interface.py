68ad3fa7010c6f617ec45e4db2f5d2c0bec47376
enskyeing <skyetsatsis@gmail.com>
2025-05-27 17:05:31 -0700

    Add GUI boilerplate

---
+++ src/graphical_user_interface.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import customtkinter as ctk
+from tkinter import ttk
+
+# ======= UI STYLE CONSTANTS ========
+
+
+class UserInterface(ctk.CTk):
+    def __init__(self) -> None:
+        super().__init__()
+        pass
+
+    def home_page(self):
+        pass
+
+    def in_game_page(self):
+        pass
+
+    def sp_loading_page(self):
+        pass
+
+    def game_setup_page(self):
+        pass
+
+    def mp_lobby_page(self):
+        pass
+
+    def mp_loading_page(self):
+        pass
+
+if __name__ == "__main__":
+    UserInterface()
