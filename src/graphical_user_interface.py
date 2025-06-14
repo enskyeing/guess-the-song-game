9b8ae2f50d9bc5031978f5ce3a4572ca538a86d9
enskyeing <skyetsatsis@gmail.com>
2025-06-13 19:52:32 -0700

    Add base home page layout

--- src/graphical_user_interface.py
+++ src/graphical_user_interface.py
@@ -3,12 +3,13 @@ import customtkinter as ctk
 from tkinter import ttk
 
 # ======= UI STYLE CONSTANTS ========
-bg = tk.PhotoImage(file="assets/music-bg.png")
+# BG = tk.PhotoImage(file="assets/music-bg.png")
+DEFAULT_FONT = ("ROBOTO", 16, "bold")
 
 
 class UserInterface(ctk.CTk):
     def __init__(self) -> None:
-        super().__init__()
+        super().__init__(fg_color="#8B80F9")
 
         # App Config
         self.title = "Guess the Song"
@@ -18,7 +19,45 @@ class UserInterface(ctk.CTk):
         self.home_page()
 
     def home_page(self):
-        pass
+        # Divider Frames
+        top_frame = ctk.CTkFrame(
+            self,
+            width=900,
+            height=350,
+            fg_color="transparent"
+        )
+        top_frame.grid(column=0, columnspan=5, row=0, rowspan=3)
+
+        # Buttons
+        sp_btn = ctk.CTkButton(
+            self, 
+            text="SINGLEPLAYER", 
+            font=DEFAULT_FONT,
+            text_color="#000000",
+            fg_color="#6CD4FF",
+            hover_color="#7CAAFC",
+            border_color="#7CAAFC",
+            border_width=1,
+            width=200,
+            height=50
+            )
+        
+        sp_btn.grid(column=1, row=3)
+
+        mp_btn = ctk.CTkButton(
+            self, 
+            text="MULTIPLAYER", 
+            font=DEFAULT_FONT,
+            text_color="#000000",
+            fg_color="#6CD4FF",
+            hover_color="#7CAAFC",
+            border_color="#7CAAFC",
+            border_width=1,
+            width=200,
+            height=50
+            )
+        
+        mp_btn.grid(column=3, row=3)
 
     def in_game_page(self):
         pass
