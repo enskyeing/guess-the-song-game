c52645e01455c36be7375893002d3f60e6407fad
enskyeing <skyetsatsis@gmail.com>
2025-06-16 00:01:35 -0700

    Dim background and change primary color to orange

--- src/graphical_user_interface.py
+++ src/graphical_user_interface.py
@@ -4,13 +4,14 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 
 # ======= UI STYLE CONSTANTS ========
-TITLE_FONT = ("ROBOTO", 60, "bold")
-DEFAULT_FONT = ("ROBOTO", 16, "bold")
+TITLE_FONT = ("ARIAL", 60, "bold")
+DEFAULT_FONT = ("CALIBRI", 20, "bold")
+BG = "assets/music-bg-3.png"
 
 
 class UserInterface(ctk.CTk):
     def __init__(self) -> None:
-        super().__init__(fg_color="#8B80F9")
+        super().__init__(fg_color="#1E0254")
 
         # App Config
         self.title("Guess the Song")
@@ -18,7 +19,7 @@ class UserInterface(ctk.CTk):
         ctk.set_default_color_theme("src/theme.json")
 
         # Background
-        self.bg = tk.PhotoImage(file="assets/music-bg-2.png")
+        self.bg = tk.PhotoImage(file=BG)
         self.canvas_bg = ctk.CTkCanvas(
             self,
             width=900,
@@ -26,14 +27,7 @@ class UserInterface(ctk.CTk):
             highlightthickness=0,
             bg="#1E0254"
             )
-        self.main_frame = ctk.CTkFrame(
-            self.canvas_bg, 
-            width=900, 
-            height=550, 
-            fg_color="transparent"
-            )
         self.canvas_bg.create_image(0, 0, image=self.bg, anchor="nw")
-        self.canvas_bg.create_window(0, 0, window=self.main_frame, anchor="nw")
         self.canvas_bg.grid(column=0, columnspan=5, row=0, rowspan=6)
         
 
@@ -43,52 +37,43 @@ class UserInterface(ctk.CTk):
     def home_page(self):
         # TODO: Add removal of widgets from parent
 
-        # Divider Frames
-        top_frame = ctk.CTkFrame(
-            self.main_frame,
-            width=900,
-            height=350,
-            fg_color="transparent"
-        )
-        top_frame.grid(column=0, columnspan=5, row=0, rowspan=3)
-
         # Labels
         game_title = ctk.CTkLabel(
-            master=self.main_frame,
+            master=self,
             text="GUESS THE SONG",
             font=TITLE_FONT,
             text_color="black"
         )
-        game_title.grid(column=1, columnspan=3, row=1)
+        self.canvas_bg.create_text(450, 250, text="GUESS THE\nSONG", font=TITLE_FONT, anchor="center", justify="center", fill="#FFA400")
 
         # Buttons
         sp_btn = ctk.CTkButton(
-            self.main_frame, 
+            self, 
             text="SINGLEPLAYER", 
             font=DEFAULT_FONT,
             width=200,
             height=50
             )
-        sp_btn.grid(column=1, row=3)
+        self.canvas_bg.create_window(150, 400, anchor="nw", window=sp_btn)
 
         mp_btn = ctk.CTkButton(
-            self.main_frame, 
+            self, 
             text="MULTIPLAYER", 
             font=DEFAULT_FONT,
             width=200,
             height=50
             )
-        mp_btn.grid(column=3, row=3)
+        self.canvas_bg.create_window(550, 400, anchor="nw", window=mp_btn)
 
         settings_icon = ctk.CTkImage(Image.open("assets/settings-icon.png"), size=(35, 35))
         settings_btn = ctk.CTkButton(
-            self.main_frame,
+            self,
             text="",
             width=50,
             height=50,
             image=settings_icon
         )
-        settings_btn.grid(column=4, row=0)
+        self.canvas_bg.create_window(800, 50, anchor="nw", window=settings_btn)
 
     def in_game_page(self):
         pass
